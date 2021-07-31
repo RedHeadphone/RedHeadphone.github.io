3842a6d7e39ee2bfc4831a503df58f57363a4a2d
RedHeadphone <khilawala92@gmail.com>
2021-07-31 13:20:16 +0530

    started making site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Red Headphone
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startAnimationDone:false,
+      totype:"Welcome to RedHeadphone's Site",
+      text:"",
+      cursor:'|'
+    };
+  }
+  cursorfunc = ()=>{
+    if(this.state.cursor==='|'){
+      this.setState({cursor:" "})
+    }
+    else{
+      this.setState({cursor:"|"})
+    }
+  }
+  type = ()=>{
+    this.setState({text:this.state.totype.slice(0,this.state.text.length+1)})
+    if (this.state.text===this.state.totype) return
+    setTimeout(this.type,Math.floor(Math.random()*500));
+}
+  onAnimationEnd = () => {
+    this.setState({
+      startAnimationDone:true
+    });
+    setInterval(this.cursorfunc,1000);
+    setTimeout(this.type,2000);
+  };
+  render(){
+    return (
+      <div className="App">
+        {!this.state.startAnimationDone && <div id="glitchEffect">
+          <div className="glitchBar" id="g1"></div>
+          <div className="glitchBar" id="g2"></div>
+          <div className="glitchBar" id="g3"></div>
+          <div className="glitchBar" id="g4"></div>
+          <div className="glitchBar" id="g5" onAnimationEnd={this.onAnimationEnd}></div>
+        </div>}
+        {this.state.startAnimationDone && <div id="content">
+          <div className="typing"><span id="text">{this.state.text}</span><span id="cursor">{this.state.cursor}</span></div>
+        </div>}
+      </div>
+    );
+  }
 }
 
 export default App;
